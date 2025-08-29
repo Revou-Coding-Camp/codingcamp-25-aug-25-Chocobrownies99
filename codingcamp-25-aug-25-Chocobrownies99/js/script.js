f1d92433b1896f75c5d429ec504efb85de246b18
Amanda Ismayawati <amandaismaya@gmail.com>
2025-08-29 18:00:11 +0700

    Add files via upload

---
+++ codingcamp-25-aug-25-Chocobrownies99/js/script.js
@@ -0,0 +1,21 @@
+console.log("Hello, World!");
+
+ greet();
+/// greet the user
+function greet() {
+    let username = prompt("Please Enter Your Name:");
+    alert("Hello,"+username+"!");
+    document.getElementById('username').innerText = username;
+}
+
+/// Validate form 
+function validateForm() {
+    let name = document.getElementById('name-input').value;
+    if (name === "") {
+        alert("name must be filled out");
+    }
+    else{
+        alert("terimakasih, " + nameInput + " Pesanmu Sudah Terkirim.");
+        document.getElementById("name-input").value= "";
+    }
+}
